428f1295a711c30a431992071902f5e89bd0f4b3
akamehash <akameshimori@gmail.com>
2022-07-04 09:22:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,45 +5,4 @@ import os, sys
 def get_user_name():
     return 'John'
 
-st.header("Echo Function: ")
-with st.echo():
-    # ----------------------------------------
-    # Want people to see this part of code...
-        # Everything inside this block will be both printed to the screen and executed.
-
-    def get_perctuation():
-        return '!!!'
-
-    greeting = "Hi three, "
-    user_name = get_user_name()
-    puntuation = get_perctuation()
-
-    st.write(greeting,user_name,puntuation)
-
-    # ...up to here
-    # ----------------------------------------
-
-# And now we're back to _not_ printing to the screen
-foo = 'bar'
-st.write('Done!')
-
-with st.spinner('Wait for it...'):
-    time.sleep(1)
-st.success(os.getenv("QUERY_STRING"))
-
-st.error('This is an error')
-
-st.warning('This is a warning')
-
-st.info('This is a purely informational message')
-
-e = RuntimeError('This is an exception of type RuntimeError')
-st.exception(e)
-
-my_placeholder = st.empty()
-
-
-st.help(pd.DataFrame)
-
-x = get_user_name()
-st.help(x)
+st.header(os.getenv("QUERY_STRING"))
