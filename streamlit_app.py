7cc5a035fcd2cdf4d675dde21ef5853793c061f7
akamehash <akameshimori@gmail.com>
2022-07-04 09:29:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import cgi
-form = cgi.FieldStorage()
 
 my_placeholder = st.empty()
-my_placeholder.text(form["username"]+'<form action="" method="get"><input type="text" name="username"><input type="submit" value="Submit"></form> ')
+my_placeholder.text('<form action="" method="get"><input type="text" name="username"><input type="submit" value="Submit"></form>')
