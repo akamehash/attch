97b4ac71e971f7782635c71d2ff92fb55914f998
akamehash <akameshimori@gmail.com>
2022-07-04 09:31:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
-my_placeholder = st.empty()
-my_placeholder.text('<form action="" method="get"><input type="text" name="username"><input type="submit" value="Submit"></form>')
+code = ''' <html>
+    <body>
+        <title>Hello World</title>
+        <meta charset="UTF-8">
+   </body>
+   <head>
+   <form action="" method="get"><input type="text" name="username"><input type="submit" value="Submit"></form><br>
+        <h1>Hello Example!</h1>
+        <br>
+        <p>Hello world</p>
+    </head>
+</html>'''
+st.code(code,language='html')
+
+with st.echo():
