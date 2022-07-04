f9283b17e9bf19cccae736ee9253f13fc949f8e8
akamehash <akameshimori@gmail.com>
2022-07-04 09:43:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import os, sys
 
+GET={}
+args=os.getenv("QUERY_STRING").split('&')
+
+for arg in args: 
+    t=arg.split('=')
+    if len(t)>1: k,v=arg.split('='); GET[k]=v
+
 my_placeholder = st.empty()
-my_placeholder.text("The user entered %s" % os.getenv("QUERY_STRING") +'<br><form action="" method="POST"><input type="text" name="username"><input type="submit" value="Submit"></form> ')
+my_placeholder.text("The user entered %s" % GET.get('user_id') +'<br><form action="" method="POST"><input type="text" name="username"><input type="submit" value="Submit"></form> ')
 
