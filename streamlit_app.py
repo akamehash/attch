a53e9daa309fc7804123ee0fb25e0f4212a9fb81
akamehash <akameshimori@gmail.com>
2022-07-04 09:32:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ code = ''' <html>
         <meta charset="UTF-8">
    </body>
    <head>
-   <form action="" method="get"><input type="text" name="username"><input type="submit" value="Submit"></form><br>
+
         <h1>Hello Example!</h1>
         <br>
         <p>Hello world</p>
