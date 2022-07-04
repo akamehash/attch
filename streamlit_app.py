c2c838928b4da38c2c2d101b9f460ecd75e7484b
akamehash <akameshimori@gmail.com>
2022-07-04 09:29:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import cgi
 form = cgi.FieldStorage()
-st.header(form["username"])
 
 my_placeholder = st.empty()
-my_placeholder.text('<form action="" method="get"><input type="text" name="username"><input type="submit" value="Submit"></form> ')
-
+my_placeholder.text(form["username"]+'<form action="" method="get"><input type="text" name="username"><input type="submit" value="Submit"></form> ')
